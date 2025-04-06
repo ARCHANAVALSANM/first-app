db36864e43b371aa342a6eb73eb8c76197dcb38e
archana <archanavalsan2000@gmail.com>
2025-04-06 20:56:16 +0530

    basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, HomeComponent],
+  // templateUrl: './app.component.html',
+  template: `
+    <main>
+      <header class="brand-name">
+        <img
+          class="brand-logo"
+          src="/assets/cart.png"
+          alt="logo"
+          aria-hidden="true"
+        />
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'first-app';
+  title = 'homes';
 }
